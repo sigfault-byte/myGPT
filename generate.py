a5dd523e08274c11206e4fd408da4b759c0c5ac1
sigfault-byte <alexandre.decampou@icloud.com>
2026-03-24 14:12:00 +0000

    clean commit without checklpoitn

--- generate.py
+++ generate.py
@@ -4,7 +4,7 @@ from config_small import device
 from models.myGPT import GPTLanguageModel
 from tokenizers.bigram_tokenizer import CharTokenizer
 
-checkpoint_path = "runs/20260324-100729_bs32_blk128_emb64_h4_l2/checkpoint_last.pt"
+checkpoint_path = "runs/20260324-133222_bs64_blk256_emb384_h6_l6/checkpoint_last.pt"
 
 checkpoint = torch.load(checkpoint_path, map_location=device)
 
@@ -17,7 +17,7 @@ model = GPTLanguageModel(checkpoint["vocab_size"]).to(device)
 model.load_state_dict(checkpoint["model_state_dict"])
 model.eval()
 
-prompt = "La libérté"
+prompt = "la religion est"
 context = torch.tensor(tokenizer.encode(prompt), dtype=torch.long, device=device).view(
     1, -1
 )
