3430bf064961914c2c54e52284a42db53f8482ab
sigfault-byte <alexandre.decampou@icloud.com>
2026-03-25 16:49:00 +0000

    fixing bugs from macos

--- tokenizer_bpe.py
+++ tokenizer_bpe.py
@@ -1,8 +1,9 @@
-from tokenizers import Tokenizer
 from tokenizers.decoders import ByteLevel as ByteLevelDecoder
 from tokenizers.models import BPE
 from tokenizers.pre_tokenizers import ByteLevel
 from tokenizers.trainers import BpeTrainer
+
+from tokenizers import Tokenizer
 
 # Path to corpus
 files = ["data/rousseau_pol_and_emile_vol1-4-5.txt"]
@@ -16,7 +17,7 @@ tokenizer.decoder = ByteLevelDecoder()
 
 # Trainer
 trainer = BpeTrainer(
-    vocab_size=512,
+    vocab_size=2048,
     min_frequency=3,
     special_tokens=["[UNK]"],
 )
@@ -25,7 +26,7 @@ trainer = BpeTrainer(
 tokenizer.train(files, trainer)
 
 # Save
-tokenizer.save("rousseau_bpe_vol1-4-5.json")
+tokenizer.save("rousseau_bpe2048_vol1-4-5.json")
 
 print("Tokenizer trained and saved!")
 print("Vocab size:", tokenizer.get_vocab_size())
